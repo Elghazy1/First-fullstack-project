39c77a7140926d00dc3f8e9dc7f1e0bfe62c6616
Elghazy1 <yazidelghazy@gmail.com>
2021-09-25 10:23:47 +0200

    Connect to database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://yazidpro:yazid_01@cluster0.rgtay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
+//mongoose.connect(CONNECTION_URL).then(()=>{console.log('...')})
