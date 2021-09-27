96e6a8fcc8414e286cce8c4728e8377d099a5209
Elghazy1 <yazidelghazy@gmail.com>
2021-09-27 12:52:05 +0200

    Showing the first post

--- FullStackProject/client/src/components/Posts/Posts.js
+++ FullStackProject/client/src/components/Posts/Posts.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+  
+
 
 
 
@@ -14,12 +17,16 @@ const Posts = () =>{
     console.log(posts);
 
     return(
-        <>
-            <h1>POSTS</h1>
-            <Post/>
-            <Post/>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+              {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                  <Post post={post} /*setCurrentId={setCurrentId} */  />
+                </Grid>
+              ))}
+          </Grid>
+        )
     );
-}
+};
 
 export default Posts;
